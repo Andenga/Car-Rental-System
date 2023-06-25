8ed9797568fb9f3703b26f86cf31b82f4f63a738
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-25 06:16:30 +0300

    Setting the home page

--- index.php
+++ index.php
@@ -7,9 +7,109 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>TWENDE CARS</title>
 </head>
 <body>
-    
+    <header>
+    <h1>TWENDE CARS</h1>
+    <div class = "main-menu">
+        <p>Our Deals</p>
+        <p>Services</p>
+        <p>Vehicles</p>
+            <div class="dropdown">
+            <p>Passenger Cars</p>
+            <p>Electric</p>
+            <p>4x4 SUVs</p>
+            <p>Mini Vans</p>
+            </div>
+        <p>SPecial Vehicles</p>
+            <div class="dropdown">
+            <p>Motor Homes</p>
+            <p>Campers</p>
+            <p>Luxury Cars</p>
+            </div>
+        <p>About Us</p>
+        <p>Terms And Policies</p>
+            <div class="drop-down">
+            <p>FAQs</p>
+            </div>
+        <p>Contact Us</p>
+    </div>
+    <h2>Need a rental car in Kenya? You've come to the right place.</h2>
+    <div class="searchbar">
+        <p>Whats type of car</p>
+        <div>
+            <p>Passenger Cars</p>
+            <p>4x4 SUVs</p>
+            <p>Mini Vans</p>
+        </div>
+    <p>Pickup and return location</p>
+    <p>Pickup date and time</p>
+    <p>return date and time</p>
+    <p>Same return</p>
+    <p>I am 16</p>
+    <p>City Nairobi</p>
+    <p>Search</p>
+    </div>
+    <div class="header-buttons">
+        <p>See our deals</p>
+        <p>Our Services</p>
+        <p>Contact Us</p>
+    </div>
+    </header>
+
+    <main>
+        <div>
+            <img src="/Images/bob-osias-2--Gg0gpWIA-unsplash.jpg" alt="">
+            <img src="/Images/maksym-tymchyk-bb6NaemzpGY-unsplash.jpg" alt="">
+            <img src="/Images/frankie-id7EXciI1fY-unsplash.jpg" alt="">
+            <img src="/Images/jake-blucker-OJX7gIU3E6U-unsplash.jpg" alt="">
+        </div>
+        <div>
+            <div>
+                <p>Corporate Services</p>
+                <p>Join thousands of satisfied corporate clients around Kenya</p>
+                <p>Europcar Kenya offer corporate services at many locations nationwide. It is this
+                national Iceland coverage that allows us to offer superior levels of service to 
+                our customers.</p>
+                <button>Learn More</button>
+            </div>
+        <img src="/Images/eduardo-flores-xpcUYaZtplI-unsplash.jpg" alt="">
+        </div>
+        <div>
+            <p>Kenya's largest fleet of new quality cars</p>
+            <p>Get to now a variety of car categories</p>
+            <div>
+                <p>Passenger cars</p>
+                <p>Variety of cars available from small eco-friendly to larger station wagons</p>
+                <button>Read More</button>
+                <img src="/Images/maxime-dore-lopl80HjSPs-unsplash.jpg" alt="">
+            </div>
+            <div>
+                <p>Electric</p>
+                <p>Our fleet of 100% electric vehicles just keeps growing.</p>
+                <button>Read More</button>
+                <img src="/Images/ernest-ojeh-aEytUoE1Tkc-unsplash.jpg" alt="">
+             </div>
+             <div>
+                <p>4x4 SUVs</p>
+                <p>No one offers better range of new and well maintained 4x4 SUVs</p>
+                <button> more </button>
+                <img src="/Images/zenigame-photo-oaZ9WEVW7g8-unsplash.jpg" alt="">
+             </div>
+             <div>
+                <p>Minivans</p>
+                <p>Small or large group? We cover everything from 7 to 17 seat people carriers</p>
+                <button> more</button>
+                <img src="/Images/josh-withers-hZ8tahOWVvI-unsplash.jpg" alt="">
+             </div>
+        </div>
+        <div>
+            
+        </div>
+
+
+    </main>
 </body>
 </html>
