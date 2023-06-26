d56c85ac1468aed456c3b7a64691bcba21bc45f9
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-27 02:24:06 +0300

    Home page heading

--- index.php
+++ index.php
@@ -9,11 +9,28 @@
     <link rel="stylesheet" type="text/css" href="style.css" >   
 </head>
 <body>
+    <div class="top-header">
     <header>
         <?php require "header.php"; ?>
     </header>
 
+    <h2>Need a rental car in Iceland? You've come to the right place.</h2>
+
+    <div class="header-selector">
+        
+
+    </div>
+
+    </div>
+
+
+
+
     <main></main>
+
+
+
+
     <footer>
         <?php require "footer.php" ?>
     </footer>
