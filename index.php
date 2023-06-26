29d68b082ea34ae9a3e633ae52c62271ea2abdbb
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-27 00:40:43 +0300

    Fresh start

--- index.php
+++ index.php
@@ -1,134 +1,14 @@
 
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Car Rental</title>
+    <title>Car Rentals</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
-
 <body>
-
-    <div class="topheader">
-
-    <main>
-    <h2>Need a rental car in Kenya? You've come to the right place.</h2>
-
-    <div class="searchbar">
-        <p>Whats type of car</p>
-        <div>
-            <p>Passenger Cars</p>
-            <p>4x4 SUVs</p>
-            <p>Mini Vans</p>
-        </div>
-    <p>Pickup and return location</p>
-    <p>Pickup date and time</p>
-    <p>return date and time</p>
-    <p>Same return</p>
-    <p>I am 16</p>
-    <p>City Nairobi</p>
-    <p>Search</p>
-    </div>
-
-    <div class="header-buttons">
-        <p>See our deals</p>
-        <p>Our Services</p>
-        <p>Contact Us</p>
-    </div>
-
-    </div>
-
-
-        <div>
-            <img src="/Images/bob-osias-2--Gg0gpWIA-unsplash.jpg" >
-            <img src="/Images/maksym-tymchyk-bb6NaemzpGY-unsplash.jpg" >
-            <img src="/Images/frankie-id7EXciI1fY-unsplash.jpg" >
-            <img src="/Images/jake-blucker-OJX7gIU3E6U-unsplash.jpg" >
-        </div>
-        <div>
-            <div>
-                <p>Corporate Services</p>
-                <p>Join thousands of satisfied corporate clients around Kenya</p>
-                <p>Europcar Kenya offer corporate services at many locations nationwide. It is this
-                national Iceland coverage that allows us to offer superior levels of service to 
-                our customers.</p>
-                <button>Learn More</button>
-            </div>
-        <img src="/Images/eduardo-flores-xpcUYaZtplI-unsplash.jpg">
-        </div>
-        <div>
-            <p>Kenya's largest fleet of new quality cars</p>
-            <p>Get to now a variety of car categories</p>
-            <div>
-                <p>Passenger cars</p>
-                <p>Variety of cars available from small eco-friendly to larger station wagons</p>
-                <button>Read More</button>
-                <img src="/Images/maxime-dore-lopl80HjSPs-unsplash.jpg">
-            </div>
-            <div>
-                <p>Electric</p>
-                <p>Our fleet of 100% electric vehicles just keeps growing.</p>
-                <button>Read More</button>
-                <img src="/Images/ernest-ojeh-aEytUoE1Tkc-unsplash.jpg">
-             </div>
-             <div>
-                <p>4x4 SUVs</p>
-                <p>No one offers better range of new and well maintained 4x4 SUVs</p>
-                <button> more </button>
-                <img src="/Images/zenigame-photo-oaZ9WEVW7g8-unsplash.jpg" >
-             </div>
-             <div>
-                <p>Minivans</p>
-                <p>Small or large group? We cover everything from 7 to 17 seat people carriers</p>
-                <button> more</button>
-                <img src="/Images/josh-withers-hZ8tahOWVvI-unsplash.jpg">
-             </div>
-        </div>
-        <div>
-            <div>
-                <img src="/Images/6947636_distance_travel_vacation_icon.png">
-                <p>Limited mileage</p>
-                <p>We also offer daily rates with 100 or 200 km per day</p>
-                <button>Book now</button>
-            </div>
-            <div>
-                <img src="/Images/5349749_faq_faqs_help_info_information_icon.png">
-                <p>FAQs</p>
-                <p>Questions and answers about the car rental process</p>
-                <button>Read More</button>
-            </div>
-            <div>
-                <img src="/Images/6071776_agreement_conditions_contract_policy_terms_icon.png">
-                <p>Environmental initiatives</p>
-                <p>The only ISO 14001 certified car rental in Iceland</p>
-                <button>Read More</button>
-            </div>
-        </div>
-        <div>
-            <p>What are you looking for?</p>
-            <p>Search bar</p>
-            <p>Follow Us on</p>
-            <img src="/Images/104458_facebook_social media_fb_social_icon.png">
-            <img src="/Images/1161954_instagram_icon.png">
-            <img src="/Images/104461_twitter_icon.png">
-        </div>
-        <div>
-            <p>More than 55 Years of Reliable Customer Service</p>
-            <p>Founded back in 1966 Höldur ehf - Bílaleiga Akureyrar franchisee for Europcar car 
-                rental is the largest car rental operator in Iceland. The company has many rental
-                 outlets and service stations scattered around the island. Our broad fleet of new 
-                 and well maintained cars counts more than 7000 during the high season. Swift, good 
-                 and personal customer service has been our top priority from day one.
-            </p>
-        </div>
-</main>
     
-    <footer>
-        <?php require 'footer.php'; ?>
-    </footer>
 </body>
 </html>
