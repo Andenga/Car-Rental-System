4325e6a2d860456d2720c2c3585469589514eb34
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-27 00:48:53 +0300

    Home page settings

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Rentals</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" >   
 </head>
 <body>
+    
     
 </body>
 </html>
