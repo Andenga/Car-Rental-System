8a34f75badf36996185e2e356bf67ed6d6980e66
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-27 03:19:54 +0300

    Fix grid css

--- index.php
+++ index.php
@@ -17,11 +17,10 @@
     <h2>Need a rental car in Iceland? You've come to the right place.</h2>
 
     <div class="header-selector">
-        
-
+      <p>sdmk</p>  
     </div>
 
-    <div class="header-buttons">
+    <div class="grid4">
         <a href="#">See Our Deals</a>
         <a href="#">Vehicles</a>
         <a href="#">Contact us</a>
@@ -33,7 +32,30 @@
 
 
 
-    <main></main>
+    <main>
+        <div class="grid4">
+            <img src="/Images/ernest-ojeh-aEytUoE1Tkc-unsplash.jpg" alt="Electric car">
+            <img src="/Images/maksym-tymchyk-bb6NaemzpGY-unsplash.jpg" alt="SUV">
+            <img src="/Images/frankie-id7EXciI1fY-unsplash.jpg" alt="Motorhome">
+            <img src="/Images/jake-blucker-OJX7gIU3E6U-unsplash.jpg" alt="Beautiful home">
+        </div>
+
+        <div class="flex2">
+            <div class="left">
+                <p>Corporate Services</p>
+                <p>Join thousands of satisfied corporate clients around Iceland</p>
+                <p>Europcar Iceland offer corporate services at many locations nationwide. It is this
+                  national Iceland coverage that allows us to offer superior levels of service to
+                  our customers.</p>
+                <button>Learn More</button>
+            </div>
+            <div class="right">
+                <img src="/Images/eduardo-flores-xpcUYaZtplI-unsplash.jpg" alt="Red car">
+            </div>
+        </div>
+
+
+    </main>
 
 
 
