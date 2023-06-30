c02a72242b1eb99b3273eb8c815db13a235dca9c
Andenga <62437418+Andenga@users.noreply.github.com>
2023-07-01 02:34:13 +0300

    signin, signup and login page

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Rentals</title>
-    <link rel="stylesheet" type="text/css" href="style.css" >   
+    <link rel="stylesheet" type="text/css" href="style.css" >  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
+
+    <div class="container">
+            <h1>Welcome to Dashboard</h1>
+            <a href="logout.php" class="btn btn-warning">Logout</a>
+    </div>
+
+
     <div class="top-header">
     <header>
         <?php require "header.php"; ?>
